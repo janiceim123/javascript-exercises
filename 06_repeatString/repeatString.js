cbae3341c685ef3047e08bdfbd4e5bf2c412c46b
jjanim <162180508+janiceim123@users.noreply.github.com>
2025-06-21 22:08:12 -0700

    finished 06

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, times) {
+    // for loop times times
+    //just print string 
+    if (times < 0) return "ERROR";
+    let result = "";
+    for (let i = 0; i < times; i++) {
+        result += string;
+    }
+    return result;
 };
 
 // Do not edit below this line
