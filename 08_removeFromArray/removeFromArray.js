1d69b149858fc08acc89f676e5c747428bf93c05
jjanim <162180508+janiceim123@users.noreply.github.com>
2025-06-22 19:55:43 -0700

    finished 08 first attempt

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,23 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...remove) {
+    // if remove is in array, delete it
+    // return array
+    
+    // loop thru arguments checking if included in array, 
+    // if in, remove all instances
+
+    for (let i = 0; i < remove.length; i++) {
+        if (array.includes(remove[i])) {
+            // while array still includes argument, delete from array
+            while (array.includes(remove[i])) {
+                array.splice(array.indexOf(remove[i]), 1);
+            }
+        }
+    }
+
+    return array;
+
+    // another way would just be to make a new array with all wanted items
+    // and return the new array
 };
 
 // Do not edit below this line
