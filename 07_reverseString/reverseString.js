ac1a98cac7117380bfe83925624c0ff531689e87
jjanim <162180508+janiceim123@users.noreply.github.com>
2025-06-22 17:42:54 -0700

    finished 07

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    // split it into array of characters
+    // reverse array
+    // join back together
+    let arr = string.split('');
+    let rev = arr.reverse();
+    return rev.join('');
 };
 
 // Do not edit below this line
