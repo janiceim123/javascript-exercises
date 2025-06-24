4930e53fd5608871d98f2c883f1fc3a211e13524
jjanim <162180508+janiceim123@users.noreply.github.com>
2025-06-23 21:06:02 -0700

    finished 10

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    // has to be % 4 = 0
+    // but also cannot be % 100 = 0, unless 400
+    let isLeap = true;
+    if (year % 4 === 0 && year % 100 !== 0 || year % 400 === 0) {
+        isLeap = true;
+    }
+    else {
+        isLeap = false;
+    }
+    return isLeap;
 };
 
 // Do not edit below this line
