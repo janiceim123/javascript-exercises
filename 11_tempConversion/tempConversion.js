35110c256e938fc634efa03393498dfe8a770a5e
jjanim <162180508+janiceim123@users.noreply.github.com>
2025-06-23 21:23:32 -0700

    finished 11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degree) {
+  // 32 F = 0 C
+  // C = (F - 32) * 5/9
+  // round to 1 decimal place , .toFixed(1), parseFloat to convert to num
+  let convert = (degree - 32) * (5/9);
+  return parseFloat(convert.toFixed(1));
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degree) {
+  // F = (C * 9/5) + 32
+  let convert = (degree * (9/5)) + 32;
+  return parseFloat(convert.toFixed(1));
 };
 
 // Do not edit below this line
