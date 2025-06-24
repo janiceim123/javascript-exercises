e568bb9686ff3949e9865255cc7cc6f74882a8c5
jjanim <162180508+janiceim123@users.noreply.github.com>
2025-06-23 20:47:03 -0700

    finished 09

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -2,9 +2,9 @@ const reverseString = function(string) {
     // split it into array of characters
     // reverse array
     // join back together
-    let arr = string.split('');
+    let arr = string.split("");
     let rev = arr.reverse();
-    return rev.join('');
+    return rev.join("");
 };
 
 // Do not edit below this line
--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,4 +1,26 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    // 1, 5
+    // get first number, add to total
+    // increment number, add new number to total
+    // repeat until reach last number, include last in total
+    // if start is larger, switch start and end
+    if (start > end) { 
+        let temp = start;  
+        start = end;  
+        end = temp;  
+    }
+    else if (start < 0 || end < 0) {
+        return "ERROR";
+    }
+    else if (!Number.isInteger(start) || !Number.isInteger(end)) {
+        return "ERROR";
+    }
+
+    let sum = 0;
+    for (let num = start; num <= end; num++) {
+        sum += num;
+    }
+    return sum;
 
 };
 
